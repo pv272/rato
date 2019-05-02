e936b1e7655c782f82523f2b4e827062a887de69
vullioud <colino.vullioud@gmail.com>
2019-05-02 15:56:55 +0200

    add function to compute elo fixed K

---
+++ rato/R/compute_elo.R
@@ -0,0 +1,53 @@
+##################################################################################################
+#' compute_elo_fixed_K
+#'
+#' return a nested data frame by colony. with interaction and elo
+#' scores before and after the interaction.
+#'
+#'@name compute_elo_fixed_K
+#'@aliases compute_elo_fixed_K
+#'@param elo_DF a data frame created by the function get_all_call_tidy()
+#'and filter_all_data().
+#'@param K the log of the K value
+#'@param nested_output logical to return a nested df by colony default = FALSE
+#'@return a tibble
+#'@import purrr
+#'@import dplyr
+#'@import EloOptimized
+#'@export
+#'@examples
+#' con <- con <- DBI::dbConnect(RMySQL::MySQL(), user = 'philippev', password = getPass::getPass(),
+#' dbname = 'Moleratdatabase', host = 'Kalahariresearch.org')
+#' AllCall_Tidy <- get_all_call_tidy(con)
+#' Elo_Data <- filter_all_data(AllCall_Tidy, 15)
+#' test <- compute_elo_fixed_K(Elo_Data, K = 4)
+
+compute_elo_fixed_K <- function(elo_DF, K, nested_output = FALSE){
+  d2 <- elo_DF %>% nest(-Colony) %>%
+   mutate(all_ids = purrr::map(data, ~ unique(c(.x$Winner, .x$Loser))))
+out1 <- d2 %>%
+  mutate(new_df = purrr::pmap(list(X = d2$data, Y = d2$all_ids, Z = K), function(X, Y, Z) {
+          out <- EloOptimized::elo.model1(par = Z,
+                            burn_in = 10,
+                            init_elo = 1000,
+                            IA_data = X,
+                            all_ids = Y,
+                            return_likelihood =F)
+          }
+     ))
+
+### specify the output
+if (nested_output) {
+
+  final_df <- out1
+
+} else {
+  out1 <- out1 %>% mutate(new_df = purrr::map2(new_df, Colony, ~ .x %>% mutate(Colony = .y)))
+
+  final_df <- bind_rows(out1$new_df)
+}
+return(final_df)
+}
+
+
+
